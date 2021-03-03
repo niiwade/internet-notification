b9e87cfc1d91706e27437b56a23c9595c257fe21
niiwade <niilantewade77@gmail.com>
2021-03-03 22:49:15 +0000

    second psuh

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 // select all required elements
 
-const box = document.querySelector(".box");
-const pop = document.querySelector(".pop");
-const wifiIcon = document.querySelector(".icon");
-const title = document.querySelector("span");
-const subTitle = document.querySelector("p");
+const box = document.querySelector(".box"),
+    pop = box.querySelector(".pop"),
+    wifiIcon = box.querySelector(".icon"),
+    title = box.querySelector("span"),
+    subTitle = box.querySelector("p"),
+    closeIcon = box.querySelector(".close-icon");
 
 window.onload = () => { //once window loads code runs
     function ajax() {
@@ -19,17 +20,26 @@ window.onload = () => { //once window loads code runs
                 subTitle.innerText = "Yo ! Internet is connected";
                 wifiIcon.innerHTML = '<i class="uil uil-wifi"></i>';
 
+                closeIcon.onclick = () => {
+                    box.classList.add("hide");
+                }
+
+                setTimeout(() => {
+                    box.classList.add("hide");
+                }, 5000); // after 5 seconds it will hidden automatically
+
             } else { // user isnt online 
                 offline();
             }
-        };
+        }
         xhr.onerror = () => { //if the URL is incorrect or return a 400 or any other error
             offline();
-        };
+        }
         xhr.send();
     }
 
     function offline() { // offline function
+        box.classList.remove("hide"); // if user goes offline , it will show again
         pop.classList.add("offline");
         title.innerText = "You are offline now";
         subTitle.innerText = "Yo ! Internet is down";
